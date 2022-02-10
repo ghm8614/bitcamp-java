cd62c56b9e18dca287bb68a63d6c9c6119ce5b74
ghm8614 <ghm8614@gmail.com>
2022-02-11 00:03:11 +0900

    Update Test3.java: 중복제거

--- com/bitcamp/web/java/test/Test3.java
+++ com/bitcamp/web/java/test/Test3.java
@@ -3,21 +3,34 @@ package day01.test;
 public class Test3 {
     public static void main(String[] args) {
 
-        int i = Integer.parseInt(args[0]);
-        int j = Integer.parseInt(args[1]);
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
 
-        // i
-        if (i % 2 == 0) {
-            System.out.printf((i % 3 == 0) ? "%d: 짝수이며 3의 배수이다.\n" : "%d: 짝수이며 3의 배수가 아니다.\n", i);
+        // num1
+        if (num1 % 2 == 0) {
+            System.out.printf((num1 % 3 == 0) ? "%d: 짝수이며 3의 배수이다.\n" : "%d: 짝수이며 3의 배수가 아니다.\n", num1);
         } else {
-            System.out.printf((i % 3 == 0) ? "%d: 홀수이며 3의 배수이다.\n" : "%d: 홀수이며 3의 배수가 아니다.\n", i);
+            System.out.printf((num1 % 3 == 0) ? "%d: 홀수이며 3의 배수이다.\n" : "%d: 홀수이며 3의 배수가 아니다.\n", num1);
         }
 
-        // j
-        if (j % 2 == 0) {
-            System.out.printf((j % 3 == 0) ? "%d: 짝수이며 3의 배수이다.\n" : "%d: 짝수이며 3의 배수가 아니다.\n", j);
+        // num2
+        if (num2 % 2 == 0) {
+            System.out.printf((num2 % 3 == 0) ? "%d: 짝수이며 3의 배수이다.\n" : "%d: 짝수이며 3의 배수가 아니다.\n", num2);
         } else {
-            System.out.printf((j % 3 == 0) ? "%d: 홀수이며 3의 배수이다.\n" : "%d: 홀수이며 3의 배수가 아니다.\n", j);
+            System.out.printf((num2 % 3 == 0) ? "%d: 홀수이며 3의 배수이다.\n" : "%d: 홀수이며 3의 배수가 아니다.\n", num2);
         }
+
+        // 반복문 사용
+		/*
+		int[] intArray = new int[]{num1,num2};
+
+		for (int i = 0; i<intArray.length ; i++)
+		{
+			if(intArray[i] % 2 == 0){
+				System.out.printf((intArray[i]%3==0)?"%d: 짝수이며 3의 배수이다.\n":"%d: 짝수이며 3의 배수가 아니다.\n",intArray[i]);
+			}else{
+				System.out.printf((intArray[i]%3==0)?"%d: 홀수이며 3의 배수이다.\n":"%d: 홀수이며 3의 배수가 아니다.\n",intArray[i]);
+			}
+		}*/
     }
 }
