8e640c08bf1ba0e09db91556fe62c3e99ae76a66
ghm8614 <ghm8614@gmail.com>
2022-02-11 00:00:52 +0900

    Update Example1.java: switch-case

--- com/bitcamp/web/java/test/Example1.java
+++ com/bitcamp/web/java/test/Example1.java
@@ -5,21 +5,43 @@ public class Example1 {
 
         int score = Integer.parseInt(args[0]);
 
-        // 조건에 따라 출력
-        if (score < 0 || score > 100) {
-            System.out.println("잘못 입력하셨습니다.");
-        } else {
-            if (score >= 90) {
-                System.out.printf("입력하신 값 %d점은 수입니다.", score);
-            } else if (score >= 80) {
-                System.out.printf("입력하신 값 %d점은 우입니다.", score);
-            } else if (score >= 70) {
-                System.out.printf("입력하신 값 %d점은 미입니다.", score);
-            } else if (score >= 60) {
-                System.out.printf("입력하신 값 %d점은 양입니다.", score);
+//        // 조건에 따라 출력
+//        if (score < 0 || score > 100) {
+//            System.out.println("잘못 입력하셨습니다.");
+//        } else {
+//            if (score >= 90) {
+//                System.out.printf("입력하신 값 %d점은 수입니다.", score);
+//            } else if (score >= 80) {
+//                System.out.printf("입력하신 값 %d점은 우입니다.", score);
+//            } else if (score >= 70) {
+//                System.out.printf("입력하신 값 %d점은 미입니다.", score);
+//            } else if (score >= 60) {
+//                System.out.printf("입력하신 값 %d점은 양입니다.", score);
+//            } else {
+//                System.out.printf("입력하신 값 %d점은 가입니다.", score);
+//            }
+
+            // switch-case 사용
+            if (score < 0 || score > 100) {
+                System.out.println("잘못 입력하셨습니다.");
             } else {
-                System.out.printf("입력하신 값 %d점은 가입니다.", score);
+                switch (score / 10) {
+                    case 10:
+                    case 9:
+                        System.out.printf("입력하신 값 %d점은 수입니다.", score);
+                        break;
+                    case 8:
+                        System.out.printf("입력하신 값 %d점은 우입니다.", score);
+                        break;
+                    case 7:
+                        System.out.printf("입력하신 값 %d점은 미입니다.", score);
+                        break;
+                    case 6:
+                        System.out.printf("입력하신 값 %d점은 양입니다.", score);
+                        break;
+                    default:
+                        System.out.printf("입력하신 값 %d점은 가입니다.", score); // break 생략가능
+                }
             }
         }
     }
-}
