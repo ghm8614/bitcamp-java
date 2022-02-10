515bac9fd304793ab179f0e8b3cbe7399cafcc78
ghm8614 <ghm8614@gmail.com>
2022-02-11 00:01:57 +0900

    Update Example2.java

--- com/bitcamp/web/java/test/Example2.java
+++ com/bitcamp/web/java/test/Example2.java
@@ -4,18 +4,36 @@ package day02.test;
 class Example2 {
     public static void main(String[] args) {
 
-        // count 배열 생성
-        int[] countArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-
+        // 1. dan 입력받기
         int dan = Integer.parseInt(args[0]);
+
+        // 2. count를 for문으로
+        if (dan <= 0 | dan >= 10) {
+            System.out.println("다시 실행하세요.");
+        } else {
+            System.out.println(dan + "단을 출력합니다.");
+            for (int count = 1; count < 10; count++) {
+                System.out.printf("%d * %d = %d\n", count, dan, count * dan);
+            }
+        }
+
+        /*
+        // 배열 이용
+
+        // 1. dan 입력받기
+        int dan = Integer.parseInt(args[0]);
+
+        // 2. count를 배열로
+        int[] intArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         if (dan <= 0 || dan >= 10) {
             System.out.println("다시 실행하세요.");
         } else {
             System.out.println(dan + "단을 출력합니다.");
-            for (int i = 0; i < countArray.length; i++) {
+            for (int i = 0; i < intArray.length; i++) {
                 System.out.printf("%d * %d = %d\n", dan, i + 1, dan * (i + 1));
             }
         }
+         */
     }
 }
