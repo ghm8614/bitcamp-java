7bbf6e3a0548ef00b7aa43d6983c278c2eb87fbc
ghm8614 <ghm8614@gmail.com>
2022-02-22 23:08:56 +0900

    Update Adder.java

--- com/bitcamp/web/java/prob/Adder.java
+++ com/bitcamp/web/java/prob/Adder.java
@@ -17,18 +17,31 @@ public class Adder {
             } else {
                 strNum += c;
             }
-        }
+        }//end of for
 
         return (result + Integer.parseInt(strNum));  // 더하기가 없는 맨 마지막 숫자를 더해줘야 한다
+    }
+
+    public int execute2(String expr) {
+//        String expr = "3+5+9+1";
+
+        int result = 0;
+
+        String[] strArr = expr.split("\\+");
+
+        for (int i = 0; i < strArr.length; i++) {
+            result += Integer.parseInt(strArr[i]);
+        }//end of for
+        return result;
     }
 
     public static void main(String[] args) {
         Adder adder = new Adder();
         String expr = "3+5+9+1";
-        System.out.println(expr + "=" + adder.execute(expr));
+        System.out.println(expr + "=" + adder.execute2(expr));
         expr = "11+45+77+3";
-        System.out.println(expr + "=" + adder.execute(expr));
+        System.out.println(expr + "=" + adder.execute2(expr));
         expr = "33+51+12+11";
-        System.out.println(expr + "=" + adder.execute(expr));
+        System.out.println(expr + "=" + adder.execute2(expr));
     }
 }
